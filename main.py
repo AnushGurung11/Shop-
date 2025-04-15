32b1a0ad76af780d1194988ad883cc219df156ed
AnushGurung11 <anushgrg15@gmai.com>
2025-04-15 22:37:20 +0545

    Third Commit

--- main.py
+++ main.py
@@ -5,8 +5,20 @@ def read_file(filename):
     
     #By default the file always opens in read mode 
     file = open(filename,"r")
+    items2D = []
     
-    print(file.readlines())
+    for each in file.readlines():
+        new_list = each.strip().split(",")
+        items2D.append(new_list)
+        
+    for product in items2D: 
+         print(f"{product[0]:<20} {product[1]:<20} {product[2]:<20} {product[3]:<20} {product[4]:<20}")
+
+    
+        
+    
+    
+    
 
 
 def display_products():
@@ -16,7 +28,7 @@ def display_products():
     #printing the column headers with left align and dedicated space of 20 
     print(f"{header[0]:<20}{header[1]:<20}{header[2]:<20}{header[3]:<20}{header[4]:<20}")
     # A line to divide the header form the content 
-    print("-"*70)
+    print("-"*100)
     
     
 
